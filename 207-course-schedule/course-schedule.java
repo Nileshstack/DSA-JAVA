8497d97719f21924058c14d18b873da3c888c3fe
Nilesh Srivastava <nileshsrivastava.stjoseph@gmail.com>
2026-08-23 02:01:09 +0530

    Time: 6 ms (71.99%) | Memory: 47 MB (78.12%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,34 +1,38 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> graph = new ArrayList<>();
-        int[] degree = new int[numCourses];
-        
-        for(int i=0; i<numCourses; i++){
-            graph.add(new ArrayList<>());
+    public boolean canFinish(int V, int[][] edges) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<V;i++){
+            adj.add(new ArrayList<>());
         }
+        int indegree[]= new int[V];
         
-        for(int[] pre : prerequisites){
-            graph.get(pre[1]).add(pre[0]);
-            degree[pre[0]]++;   
+        for(int edge[] : edges){
+            int u= edge[0];
+            int v= edge[1];
+            //directed isliye dono direction me add karo 
+            adj.get(u).add(v);
+            
+            indegree[v]++;
         }
-        
+        int c=0;
         Queue<Integer> que = new LinkedList<>();
-        for(int i=0; i<numCourses; i++){
-            if(degree[i] == 0){
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0){
                 que.offer(i);
+                c++;
             }
         }
         
-        int c = 0;
         while(!que.isEmpty()){
-            int curr = que.poll();
-            c++;
-            for(int next : graph.get(curr)){
-                degree[next]--;
-                if(degree[next] == 0) que.offer(next);
+            int u= que.poll();
+            for(int nei: adj.get(u)){
+                indegree[nei]--;
+                if(indegree[nei]==0){
+                que.offer(nei);
+                c++;
+                }
             }
         }
-        
-        return c == numCourses;  
+        return c==V?true:false;
     }
 }
