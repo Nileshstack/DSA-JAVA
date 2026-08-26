e2b356dc03c304e3650c8076f35100acaddff2bb
Nilesh Srivastava <nileshsrivastava.stjoseph@gmail.com>
2026-08-27 00:27:52 +0530

    Time: 15 ms (64.57%) | Memory: 47.8 MB (32.93%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,75 +1,42 @@
-import java.util.*;
-
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-
-        int n = grid.length;
-
-        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1)
-            return -1;
-
+       int n= grid.length;
+       int m = grid[0].length;
+       if(n==0 || m==0 || grid[0][0]!=0 || grid[n-1][n-1] == 1){
+        return -1;
+       } 
+        //store the 4 direction
         int[][] directions = {
-                {-1,-1}, {-1,0}, {-1,1},
-                {0,-1},          {0,1},
-                {1,-1}, {1,0},   {1,1}
-        };
-
-        // distance array
-        int[][] dist = new int[n][n];
-
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
-        }
-
-        dist[0][0] = 1;
-
-        
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-                (a, b) -> a[0] - b[0]
-        );
-
-        pq.offer(new int[]{1, 0, 0});
-
-        while (!pq.isEmpty()) {
-
-            int[] current = pq.poll();
-
-            int distance = current[0];
-            int row = current[1];
-            int col = current[2];
-
-            if (row == n - 1 && col == n - 1)
-                return distance;
-
-            // Ignore outdated entries
-            if (distance > dist[row][col])
-                continue;
-
-            for (int[] dir : directions) {
-
-                int newRow = row + dir[0];
-                int newCol = col + dir[1];
-
-                if (newRow >= 0 && newRow < n &&
-                    newCol >= 0 && newCol < n &&
-                    grid[newRow][newCol] == 0) {
-
-                    int newDistance = distance + 1;
-
-                    if (newDistance < dist[newRow][newCol]) {
-
-                        dist[newRow][newCol] = newDistance;
-
-                        pq.offer(new int[]{
-                                newDistance,
-                                newRow,
-                                newCol
-                        });
-                    }
+        {-1,-1}, {-1,0}, {-1,1},
+        {0,-1},           {0,1},
+        {1,-1},  {1,0},   {1,1}
+       };
+       int dist=1;
+       grid[0][0]=1;
+        Queue<int[]> pq = new LinkedList<>();
+        pq.offer(new int[]{0,0}); 
+        while(!pq.isEmpty()){
+            int N= pq.size();
+            while(N>0){
+             int []curr= pq.poll();
+             int row=curr[0];
+             int col=curr[1];
+             if(row==n-1 && col==n-1){
+                return dist;
+             }
+             for(int[]dir : directions){
+               int newrow=row+dir[0];
+               int newcol=col+dir[1];
+                if(newrow>=0 && newrow<n &&newcol>=0 && newcol<n && grid[newrow][newcol]==0){
+                    grid[newrow][newcol]=1;
+                    pq.offer(new int[]{newrow,newcol});
                 }
+             }
+                N--;
             }
+            dist++;
+            
         }
-
         return -1;
     }
 }
