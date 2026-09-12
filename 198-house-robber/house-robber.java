3d6dd027a335e44f113be80d2b4599e2577ee274
Nilesh Srivastava <nileshsrivastava.stjoseph@gmail.com>
2026-09-13 02:20:10 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (5.95%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,15 +1,16 @@
 class Solution {
-    public int solve(int[]nums,int []dp, int i,int n){
-        if(i>=n) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int steel= nums[i]+solve(nums,dp,i+2,n);
-        int skip=solve(nums,dp,i+1,n);
-        return dp[i]=Math.max(steel,skip);
-    }
     public int rob(int[] nums) {
+        //Bottom-up approach
         int n=nums.length;
-        int[]dp= new int[n+1];
-        Arrays.fill(dp,-1);
-     return solve(nums,dp,0,n);   
+        int []dp= new int[n+1];
+        Arrays.fill(dp,0);
+        dp[0]=0;//No house is steeled;
+        dp[1]= nums[0];//House 1 is steeled
+        for(int i=2;i<=n;i++){
+            int steel=nums[i-1]+dp[i-2];
+            int skip=dp[i-1];
+            dp[i]=Math.max(steel,skip);
+        }   
+        return dp[n];
     }
 }
