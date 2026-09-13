bba570c4db2836383c24351856c6329208f6aafa
Nilesh Srivastava <nileshsrivastava.stjoseph@gmail.com>
2026-09-14 02:33:41 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (8.46%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,25 +1,27 @@
 class Solution {
-     public int solve(int[]nums,int []dp, int i,int n){
-        if(i>=n) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int steel= nums[i]+solve(nums,dp,i+2,n);
-        int skip=solve(nums,dp,i+1,n);
-        return dp[i]=Math.max(steel,skip);
-    }
     public int rob(int[] nums) {
-        int n=nums.length;
+       int n= nums.length;
         if(n==1){
             return nums[0];
         }
-        if(n==2){
-            return Math.max(nums[0],nums[1]);
+        int []dp= new int[n+1];
+        Arrays.fill(dp,0);
+        dp[0]=0;
+        for(int i=1;i<=n-1;i++){
+            int skip = dp[i-1];
+            int take = nums[i-1]+((i-2)>=0?dp[i-2]:0);
+            dp[i] = Math.max(skip, take);
         }
-        int[]dp1= new int[n+1];
-        Arrays.fill(dp1,-1);
-        int[]dp2= new int[n+1];
-        Arrays.fill(dp2,-1);
-        int SteelFirstHouse= solve(nums,dp1,0,n-1);
-        int SteelSecondhouse= solve(nums,dp2,1,n);
-        return Math.max(SteelFirstHouse,SteelSecondhouse);
+        int res1=dp[n-1];
+        Arrays.fill(dp,0);
+        dp[0]=0;
+        dp[1]=0;
+        for(int i=2;i<=n;i++){
+            int skip = dp[i-1];
+            int take = nums[i-1]+((i-2)>=0?dp[i-2]:0);
+            dp[i] = Math.max(skip, take);
+        }
+        int res2=dp[n];
+        return Math.max(res1,res2);
     }
 }
